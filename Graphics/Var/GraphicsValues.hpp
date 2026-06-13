081303a3d8052c853467f431890671f03b688ea2
Matheus-Sounier <matheus.sounier2024@gmail.com>
2026-06-13 15:33:13 -0400

    chore: groups every MAT of pipeline to a single square, each instance belongs to a square

--- Graphics/Var/GraphicsValues.hpp
+++ Graphics/Var/GraphicsValues.hpp
@@ -12,4 +12,20 @@ namespace GraphicsValues {
         extern cv::Mat img, imgFlip;
         extern cv::Mat JumpIMGCrop, JumpIMGGray, JumpIMGBackground, JumpIMGThres, JumpIMGSub, JumpIMGDil;
     }
+
+    namespace CVSquares {
+
+        /// Processed images for each square
+        struct Images {
+            cv::Mat imgCrop;
+            cv::Mat imgDil;
+            cv::Mat imgGray;
+            cv::Mat imgThres;
+            cv::Mat imgBackground;
+            cv::Mat imgSub;
+            int ID;
+        };
+
+        extern std::vector<Images> Frames;
+    }
 }
