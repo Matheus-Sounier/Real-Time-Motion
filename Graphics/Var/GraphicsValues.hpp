d100f000651ea6d5703322100735908aadb90c9e
Matheus-Sounier <matheus.sounier2024@gmail.com>
2026-06-13 15:55:02 -0400

    chore: creates a division line detecting moviments inside jump's area

--- Graphics/Var/GraphicsValues.hpp
+++ Graphics/Var/GraphicsValues.hpp
@@ -17,29 +17,44 @@ namespace GraphicsValues {
 
         /// Processed images for each square
         struct Images {
-            cv::Mat imgCrop;
-            cv::Mat imgDil;
-            cv::Mat imgGray;
-            cv::Mat imgThres;
-            cv::Mat imgBackground;
-            cv::Mat imgSub;
-            int ID;
+            cv::Mat imgCrop;       
+            cv::Mat imgDil;        
+            cv::Mat imgGray;       
+            cv::Mat imgThres;      
+            cv::Mat imgBackground; 
+            cv::Mat imgSub;        
+            int ID;                
         };
 
         /// Square metadata
         struct Rectangles {
-            cv::Point TL;
-            cv::Point BR;
-            cv::Scalar COLOR;
-            int KEY;
-            std::string DISPLAYKEY;
-            bool SELECTED;
-            bool MOTION_DETECTED;
-            int ID;
+            cv::Point TL;                
+            cv::Point BR;                
+            cv::Scalar COLOR;            
+            int KEY;                     
+            std::string DISPLAYKEY;      
+            bool SELECTED;               
+            bool MOTION_DETECTED;        
+            int ID;                      
         };
 
         extern std::vector<Images> Frames;
         extern std::vector<Rectangles> Squares;
         extern std::vector<std::vector<cv::Point>> contours;
+    }
+
+    namespace CVJumpLine {
+
+        struct Line {
+            cv::Point Position;
+            cv::Scalar COLOR;
+            bool SELECTED = false;
+        };
+
+        extern std::vector<Line> Lines;
+        extern bool LineExists;
+        extern const int W;
+        extern const int Y;
+        extern std::vector<std::vector<cv::Point>> mycontours;
     }
 }
